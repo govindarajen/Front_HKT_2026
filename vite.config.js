cf50a9b067fb502167537a693de7068622833a07
Govindarajen Coodien <coodien21@gmail.com>
2026-03-16 14:07:54 +0100

    change port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     host: '0.0.0.0', // listen on all IPv4 addresses
+    port: 3000, // specify the port to use
     strictPort: true // fail instead of picking a new port silently
   },
   plugins: [react()],
